bcb049a1bbed0bf5d49fab06ae0d4aed231800f0
Collin Van Dyck <collinvandyck@gmail.com>
2024-12-16 12:48:14 -0500

    wip

--- src/obs.rs
+++ src/obs.rs
@@ -1,5 +1,17 @@
-use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub fn init() {
-    tracing_subscriber::fmt().init();
+    let ef = tracing_subscriber::EnvFilter::new("error,matrix_client=info");
+    let stderr_layer = tracing_subscriber::fmt::layer()
+        .with_writer(std::io::stderr)
+        .with_thread_ids(false)
+        .with_target(true)
+        .with_file(true)
+        .with_ansi(true)
+        .with_line_number(true)
+        .without_time();
+    tracing_subscriber::registry()
+        .with(ef)
+        .with(stderr_layer)
+        .init();
 }
