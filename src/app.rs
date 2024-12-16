dc92d08ba4abcd9aeeaacf537f6dcf7dabbc0f22
Collin Van Dyck <collinvandyck@gmail.com>
2024-12-16 10:45:23 -0500

    wip

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,25 @@
-pub struct Config {}
+use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
+use std::path::Path;
 
-pub struct App {}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {
+    username: String,
+    password: String,
+    db_path: String,
+    session_path: String,
+}
+
+pub struct App {
+    config: Config,
+}
+
+impl App {
+    pub async fn start(config_p: &Path) -> Result<Self> {
+        let bs = tokio::fs::read(config_p)
+            .await
+            .context("read config")?;
+        let config: Config = serde_yaml::from_slice(bs.as_slice()).context("parse yaml")?;
+        Ok(Self { config })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+#![allow(unused)]
+
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(clap::Parser, Debug)]
+struct Args {
+    #[arg(long, default_value = "config.yaml")]
+    config: PathBuf,
+}
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    matrix_client::app::App::start(&args.config).await?;
+    Ok(())
 }
